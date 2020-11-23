6fdab115727cabb25b85c20cf143dc65ba64ca93
NHT <truongh.nguyen@thuocsi.vn>
2020-11-23 17:36:55 +0700

    add footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,10 @@
-import Image from 'next/image';
 import React from 'react';
-import Layout from '../components/Layout';
+import Image from 'next/image';
+import dynamic from 'next/dynamic';
+import { Footer } from '../components';
+
+const Layout = dynamic(() => import('../components/Layout'));
+// const { Footer } = dynamic(() => import('../components'));
 
 export default function Index() {
   const title = 'Thuocsi.vn';
@@ -9,6 +13,7 @@ export default function Index() {
     <Layout title={title}>
       <Image src="/images/logo_thuocsi.svg" width="200" height="200" />
       <h1>Landing page</h1>
+      <Footer />
     </Layout>
   );
 }
