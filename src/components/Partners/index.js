3131eb63bf24f8b8fccb59959dcb2f66e59643ec
NHT <truongh.nguyen@thuocsi.vn>
2020-11-26 13:30:57 +0700

    completed

--- src/components/Partners/index.js
+++ src/components/Partners/index.js
@@ -1,85 +1,12 @@
 import React, { useRef } from 'react';
 import Image from 'next/image';
-import clsx from 'clsx';
-import FormatQuoteIcon from '@material-ui/icons/FormatQuote';
 import Slider from 'react-slick';
-import {
-  Card,
-  makeStyles,
-  CardHeader,
-  Avatar,
-  CardContent,
-  Typography,
-  Box,
-} from '@material-ui/core';
+import { makeStyles, Box } from '@material-ui/core';
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    maxWidth: 555,
-    height: 224,
-    margin: 8,
-    boxShadow: '0 0 18px -10px',
-  },
-  media: {
-    height: 0,
-    paddingTop: '56.25%', // 16:9
-  },
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
-  },
-  large: {
-    width: '80px',
-    height: '80px',
-  },
-  CardHeader: {
-    paddingBottom: '0!important',
-    paddingTop: 24,
-  },
-  headerCustomer: {
-    position: 'relative',
-    top: -15,
-    fontSize: 20,
-    color: '#00b46e',
-    fontWeight: 'bold',
-  },
-  headerTitle: {
-    position: 'relative',
-    top: -15,
-    fontSize: 16,
-    color: '#919aa3',
-  },
-  wrapperMedia: {
-    backgroundColor: '#f4f7fc',
-  },
-  cardContent: {
-    position: 'relative',
-    top: -20,
-    paddingLeft: '112px',
-    paddingRight: '20px',
-    paddingTop: 0,
-  },
-  commentStyle: {
-    fontSize: '16px',
-    fontStyle: 'italic',
-  },
-  rotate: {
-    transform: 'rotate(180deg)',
-  },
-  quote: {
-    fontSize: '1.2rem',
-    color: '#00b46e',
-    opacity: 0.2,
-  },
+const useStyles = makeStyles(() => ({
   center: {
     textAlign: 'center',
   },
