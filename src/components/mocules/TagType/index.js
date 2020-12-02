c046a6490e7a7ec2acfda4e55142bf28deed136b
NHT <truongh.nguyen@thuocsi.vn>
2020-12-02 18:02:50 +0700

    move props to const folder

--- src/components/mocules/TagType/index.js
+++ src/components/mocules/TagType/index.js
@@ -1,127 +1,57 @@
 import React from 'react';
 import { Tag } from 'components/atoms';
 import {
-  ThumbUp,
-  Receipt,
-  FlashOn,
-  Stop,
-  PriorityHigh,
-  HighlightOff,
-  ArrowUpward,
-  ArrowDownward,
-} from '@material-ui/icons';
+  BEST_SELLER,
+  INVOICE_EXPORTABLE,
+  CLOSE_DATE,
+  PROMOTE,
+  USE_VIETNAMSE,
+  FLASH_SALE,
+  OUT_OF_STOCKS,
+  DROP_SHIP,
+  CHANGE_STYLE,
+  STOP_PRODUCING,
+  NEW,
+  PRICE_DOWN,
+  PRICE_UP,
+  ONLY_THUOCSI,
+  HARD_TO_BUY,
+} from 'constants/TagType';
 
 const TagType = ({ type, date }) => {
   switch (type) {
     case 'best_sale':
-      return (
-        <Tag
-          text="Bán chạy"
-          backgroundColor="#00b46e"
-          color="#fff"
-          icon={<ThumbUp style={{ fontSize: 13, marginRight: '0.25rem!important' }} />}
-        />
-      );
+      return <Tag {...BEST_SELLER} />;
     case 'invoice_exportable':
-      return (
-        <Tag
-          text="Hóa đơn nhanh"
-          backgroundColor="#f8f9fa"
-          color="#00b46e"
-          icon={<Receipt style={{ fontSize: 13, marginRight: '0.25rem!important' }} />}
-        />
-      );
+      return <Tag {...INVOICE_EXPORTABLE} />;
     case 'close_date':
-      return (
-        <Tag
-          text={`"Cận date:" ${date}`}
-          backgroundColor="#f8f9fa"
-          color="#f9b514"
-          icon={<Receipt style={{ fontSize: 13, marginRight: '0.25rem!important' }} />}
-        />
-      );
+      return <Tag date={date} {...CLOSE_DATE} />;
     case 'promote':
-      return <Tag text="Khuyến mãi" backgroundColor="#dc3545" color="#fff" />;
+      return <Tag {...PROMOTE} />;
     case 'use_vietnamse':
-      return <Tag text="Người Việt dùng hàng Việt" color="#00b46e" backgroundColor="#f8f9fa" />;
+      return <Tag {...USE_VIETNAMSE} />;
     case 'flash_sale':
-      return (
-        <Tag
-          text="Flash Sale"
-          backgroundColor="#f8f9fa"
-          color="#f9b514"
-          icon={<FlashOn style={{ fontSize: 13, marginRight: '0.25rem!important' }} />}
-        />
-      );
+      return <Tag {...FLASH_SALE} />;
     case 'out_of_stocks':
-      return (
-        <Tag
-          text="Tạm hết hàng"
-          backgroundColor="#f8f9fa"
-          color="#f9b514"
-          icon={<HighlightOff style={{ fontSize: 13, marginRight: '0.25rem!important' }} />}
-        />
-      );
+      return <Tag {...OUT_OF_STOCKS} />;
     case 'drop_ship':
-      return <Tag text="Dropship" backgroundColor="#f8f9fa" color="#f9b514" />;
+      return <Tag {...DROP_SHIP} />;
     case 'change_style':
-      return (
-        <Tag
-          text="Đổi mẫu"
-          backgroundColor="#f8f9fa"
-          color="#f9b514"
-          icon={<FlashOn style={{ fontSize: 13, marginRight: '0.25rem!important' }} />}
-        />
-      );
+      return <Tag {...CHANGE_STYLE} />;
     case 'stop_producing':
-      return (
-        <Tag
-          text="Ngừng sản xuất"
-          backgroundColor="#f8f9fa"
-          color="#f9b514"
-          icon={<Stop style={{ fontSize: 13, marginRight: '0.25rem!important' }} />}
-        />
-      );
+      return <Tag {...STOP_PRODUCING} />;
     case 'only_thuocsi':
-      return (
-        <Tag
-          text="Chỉ có tại thuocsi"
-          backgroundColor="#f8f9fa"
-          color="#00b46e"
-          icon={<PriorityHigh style={{ fontSize: 13, marginRight: '0.25rem!important' }} />}
-        />
-      );
+      return <Tag {...ONLY_THUOCSI} />;
     case 'hard_to_buy':
-      return (
-        <Tag
-          text="Khó mua"
-          backgroundColor="#f8f9fa"
-          color="#00b46e"
-          icon={<PriorityHigh style={{ fontSize: 13, marginRight: '0.25rem!important' }} />}
-        />
-      );
+      return <Tag {...HARD_TO_BUY} />;
     case 'new':
-      return <Tag text="Mới" backgroundColor="#f9b514" color="#212529" />;
+      return <Tag {...NEW} />;
     case 'price_down':
-      return (
-        <Tag
-          text="Giảm Giá"
-          backgroundColor="#f8f9fa"
-          color="#00b46e"
-          icon={<ArrowDownward style={{ fontSize: 13, marginRight: '0.25rem!important' }} />}
-        />
-      );
+      return <Tag {...PRICE_DOWN} />;
     case 'price_up':
-      return (
-        <Tag
-          text="Tăng Giá"
-          backgroundColor="#f8f9fa"
-          color="#dc3545"
-          icon={<ArrowUpward style={{ fontSize: 13, marginRight: '0.25rem!important' }} />}
-        />
-      );
+      return <Tag {...PRICE_UP} />;
     default:
-      return <Tag text="Khuyến mãi" color="#dc3545" />;
+      return <Tag {...BEST_SELLER} />;
   }
 };
 
