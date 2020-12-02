22bab896d6b324d484979d7c9da981001b22b407
NHT <truongh.nguyen@thuocsi.vn>
2020-12-02 17:09:55 +0700

    rename class

--- src/components/atoms/CircularButton/index.js
+++ src/components/atoms/CircularButton/index.js
@@ -1,6 +1,6 @@
 import React, { memo } from 'react';
 import PropTypes from 'prop-types';
-import { IconButton, Input } from '@material-ui/core';
+import { IconButton } from '@material-ui/core';
 import { Add, Remove } from '@material-ui/icons';
 import clsx from 'clsx';
 
@@ -8,12 +8,10 @@ import styles from './styles.module.css';
 
 const CircularButton = memo(({ plus }) => (
   <>
-    <Input
-      classes={{ root: styles.root_input, input: styles.input, focused: styles.focus }}
-      disableUnderline
-    />
     <IconButton
-      classes={{ root: plus ? clsx(styles.root, styles.plus) : clsx(styles.root, styles.minus) }}
+      classes={{
+        root: plus ? clsx(styles.button_root, styles.plus) : clsx(styles.button_root, styles.minus),
+      }}
     >
       {plus ? <Add className={styles.icon} /> : <Remove className={styles.icon} />}
     </IconButton>
