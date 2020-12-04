a7c1a28855020734c7a6db170b12727ed971277d
NHT <truongh.nguyen@thuocsi.vn>
2020-12-04 15:06:52 +0700

    update product card

--- src/components/mocules/index.js
+++ src/components/mocules/index.js
@@ -1,4 +1,5 @@
 export { default as CommonQuestion } from './CommonQuestion';
 export { default as Copyright } from './Copyright';
 export { default as TagType } from './TagType';
+// eslint-disable-next-line import/no-cycle
 export { default as ProductCard } from './ProductCard';
