f2f7d1570eb49170f566122b489ad2e65399ec34
NHT <truongh.nguyen@thuocsi.vn>
2020-12-08 17:24:42 +0700

    update product card

--- src/pages/testing/product/index.js
+++ src/pages/testing/product/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Box } from '@material-ui/core';
 import { PlusButton, MinusButton, RibbonPriceDown, RibbonPriceUp } from 'components/atoms';
 import { TagType, SearchInput, CardInfo } from 'components/mocules';
-import { ProductCardHorizontal } from 'components/organisms';
+import { ProductCardVertical, ProductCardHorizontal } from 'components/organisms';
 import getFormattedDate from 'utils/DateTimeUtils';
 import ProductClient from 'clients/ProductClient';
 
@@ -20,9 +20,9 @@ const date = getFormattedDate(new Date());
 
 const test = ({ products }) => (
   <>
-    {/* {products.map((item) => (
+    {products.map((item) => (
       <ProductCardVertical key={item.id} product={item} type="column" />
-    ))} */}
+    ))}
     <>
       <Box
         style={{
