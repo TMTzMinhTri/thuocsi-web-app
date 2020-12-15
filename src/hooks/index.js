00620accdba95e69700d1eccc8b5183132d5623d
Nguyen Thuan <nguyenchinhthuan15@gmail.com>
2020-12-15 17:11:01 +0700

    update index

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,3 +1,4 @@
 export { default as useDragDetection } from './useDragDetection';
 export { default as useIsMobile } from './useIsMobile';
 export { default as useModal } from './useModal';
+export { default as useStateCallback } from './useStateCallback';
