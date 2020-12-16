e730fda0f8bfa196559b3f98c9cade6f88a85f51
Nguyen Thuan <nguyenchinhthuan15@gmail.com>
2020-12-16 15:19:42 +0700

    Update remove utisl cookies not use

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,3 @@
-export * from './Cookies';
 export { default as DateTimeUtils } from './DateTimeUtils';
 export { default as FormarCurrency } from './FormarCurrency';
 export { default as FormDataUtils } from './FormDataUtils';
