a7c5fad33f278054c29f4f356c05ed64785b22e4
NHT <truongh.nguyen@thuocsi.vn>
2020-12-18 09:59:54 +0700

    update

--- src/components/mocules/SliderProduct/index.jsx
+++ src/components/mocules/SliderProduct/index.jsx
@@ -1,8 +1,6 @@
 import React, { useRef } from 'react';
 import Slider from 'react-slick';
-import {
-  Box,
-} from '@material-ui/core';
+import { Box } from '@material-ui/core';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { settingsProduct } from 'constants/data';
