70cbfe7fa7c2dc7469479967ec0134caeeff7b4c
NHT <truongh.nguyen@thuocsi.vn>
2020-12-22 09:14:25 +0700

    modify file config and next.config

--- config/index.js
+++ config/index.js
@@ -1,2 +1,2 @@
-export const API_HOST = process.env.API_HOST || 'http://api.v2-dev.thuocsi.vn';
+export const API_HOST = process.env.API_HOST || '/backend';
 export const MOCK_API_HOST = process.env.MOCK_API_HOST || '/api';
--- next.config.js
+++ next.config.js
@@ -5,19 +5,4 @@ if (process.env.NODE_ENV === 'DEVELOPMENT') {
   console.info = function () {};
 }
 
-module.exports = {
-  async rewrites() {
-    return [
-      // we need to define a no-op rewrite to trigger checking
-      // all pages/static files before we attempt proxying
-      {
-        source: '/:path*',
-        destination: '/:path*',
-      },
-      {
-        source: '/:path*',
-        destination: 'http://api.v2-dev.thuocsi.vn',
-      },
-    ];
-  },
-};
+module.exports = {};
