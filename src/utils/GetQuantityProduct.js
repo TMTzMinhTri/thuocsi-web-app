1a3dfb82361f9e1a9fc7e549e6ca88952934e030
An Pham <an.pham@thuocsi.vn>
2020-12-23 16:28:50 +0700

    fix eslint

--- src/utils/GetQuantityProduct.js
+++ src/utils/GetQuantityProduct.js
@@ -1,6 +1,6 @@
 const GetQuantityProduct = (products, cart) => {
   const productList = [];
-  products.data.forEach((product, index) => {
+  products.data.forEach((product) => {
     const id = product.sku;
     if (id === cart[id].sku) {
       productList.push({ ...product, quantity: cart[product.sku].quantity });
