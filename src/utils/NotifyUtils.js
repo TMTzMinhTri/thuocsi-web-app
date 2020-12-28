28ffdbc2750b440ad7964c9e835ee0e137d7e5b3
Nguyen Thuan <nguyenchinhthuan15@gmail.com>
2020-12-28 17:44:00 +0700

    Update noitfyUtils

--- src/utils/NotifyUtils.js
+++ src/utils/NotifyUtils.js
@@ -1,7 +1,23 @@
 import { toast } from 'react-toastify';
 
 const info = (text) => {
-  toast(text);
+  toast.info(text);
 };
 
-export default { info };
+const success = (text) => {
+  toast.success(text);
+};
+
+const dark = (text) => {
+  toast.dark(text);
+};
+
+const error = (text) => {
+  toast.error(text);
+};
+
+const warn = (text) => {
+  toast.warn(text);
+};
+
+export default { info, success, dark, error, warn };
