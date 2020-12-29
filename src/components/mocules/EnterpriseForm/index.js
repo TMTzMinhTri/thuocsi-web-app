d2a1e0cef6281465fb8fb14225dd28a4167455f7
Đạt Duy <dat.le@thuocsi.vn>
2020-12-29 08:01:09 +0000

    - fix address dropdown - add reponsive my-account page

--- src/components/mocules/EnterpriseForm/index.js
+++ src/components/mocules/EnterpriseForm/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Paper, Grid, Button, InputAdornment } from '@material-ui/core';
+import { Paper, Grid, Button, InputAdornment, useMediaQuery } from '@material-ui/core';
 import { InfoInput, InfoFormControl } from 'components/atoms';
 import GroupAddressSelect from '../GroupAddressSelect';
 import styles from './styles.module.css';
 
 const ButtonUploadFile = () => (
   <InputAdornment>
-    <Button color="default" variant="contained">
-      Browser
+    <Button color="default" variant="contained" classes={{ root: styles.upload_button }}>
+      Browse
     </Button>
   </InputAdornment>
 );
@@ -22,83 +22,86 @@ const EnterpriseForm = ({
   billDistrict,
   billProvince,
   handleSetValue,
-}) => (
-  <Paper className={styles.root} elevation={4}>
-    <h1 className={styles.title}> Thông tin doanh nghiệp </h1>
-    <Grid container spacing={2}>
-      <InfoFormControl xs={3} label="Bạn là" htmlFor="scope">
-        <InfoInput id="scope" value={scope} disabled />
-      </InfoFormControl>
+}) => {
+  const maxWidthScope = useMediaQuery('(max-width:600px)');
+  return (
+    <Paper className={styles.root} elevation={4}>
+      <h1 className={styles.title}> Thông tin doanh nghiệp </h1>
+      <Grid container spacing={2}>
+        <InfoFormControl xs={maxWidthScope ? 12 : 3} label="Bạn là" htmlFor="scope">
+          <InfoInput id="scope" value={scope} disabled />
+        </InfoFormControl>
 
-      <InfoFormControl xs={9} label="Tên nhà thuốc/phòng khám" htmlFor="drugstoreName">
-        <InfoInput
-          id="drugstoreName"
-          placeholder="Dược Hoàng Vũ"
-          value={drugstoreName}
-          onChange={(e) => handleSetValue('drugstoreName', e.target.value)}
-        />
-      </InfoFormControl>
+        <InfoFormControl xs={maxWidthScope ? 12 : 9} label="Tên nhà thuốc/phòng khám" htmlFor="drugstoreName">
+          <InfoInput
+            id="drugstoreName"
+            placeholder="Dược Hoàng Vũ"
+            value={drugstoreName}
+            onChange={(e) => handleSetValue('drugstoreName', e.target.value)}
+          />
+        </InfoFormControl>
 
-      <InfoFormControl xs={12} label="Tên người đại diện pháp luật" htmlFor="bussinessName">
-        <InfoInput
-          id="bussinessName"
-          placeholder="Trần Thị B"
-          value={bussinessName}
-          onChange={(e) => handleSetValue('bussinessName', e.target.value)}
-        />
-      </InfoFormControl>
-      <input type="file" hidden id="input-file" />
-      <InfoFormControl
-        xs={12}
-        label="Giấy phép kinh doanh phòng khám/nhà thuốc"
-        htmlFor="license"
-        variant="contained"
-      >
-        <InfoInput
-          id="license"
-          endAdornment={<ButtonUploadFile />}
-          component="span"
-          htmlFor="icon-button-file"
-          onClick={() => {
-            document.getElementById('input-file').click();
-          }}
-        />
-      </InfoFormControl>
-    </Grid>
-    <h1 className={styles.title}> Thông tin xuất hoá đơn </h1>
-    <Grid container spacing={2}>
-      <InfoFormControl xs={12} label="Mã số thuế" htmlFor="taxId">
-        <InfoInput
-          id="taxId"
-          placeholder="8026906145"
-          value={taxId}
-          onChange={(e) => {
-            handleSetValue('taxId', e.target.value);
-          }}
-        />
-      </InfoFormControl>
-      <InfoFormControl xs={12} label="Địa chỉ nhà thuốc/phòng khám" htmlFor="address">
-        <InfoInput
-          id="address"
-          placeholder="11 Ngô Quyền, Tân Lợi, Tp. Buôn Ma Thuột, Đắk Lắk"
-          value={bussinessAddress}
-          onChange={(e) => {
-            handleSetValue('bussinessAddress', e.target.value);
-          }}
-        />
-      </InfoFormControl>
-    </Grid>
+        <InfoFormControl xs={12} label="Tên người đại diện pháp luật" htmlFor="bussinessName">
+          <InfoInput
+            id="bussinessName"
+            placeholder="Trần Thị B"
+            value={bussinessName}
+            onChange={(e) => handleSetValue('bussinessName', e.target.value)}
+          />
+        </InfoFormControl>
+        <input type="file" hidden id="input-file" />
+        <InfoFormControl
+          xs={12}
+          label="Giấy phép kinh doanh phòng khám/nhà thuốc"
+          htmlFor="license"
+          variant="contained"
+        >
+          <InfoInput
+            id="license"
+            endAdornment={<ButtonUploadFile />}
+            component="span"
+            htmlFor="icon-button-file"
+            onClick={() => {
+              document.getElementById('input-file').click();
+            }}
+          />
+        </InfoFormControl>
+      </Grid>
+      <h1 className={styles.title}> Thông tin xuất hoá đơn </h1>
+      <Grid container spacing={2}>
+        <InfoFormControl xs={12} label="Mã số thuế" htmlFor="taxId">
+          <InfoInput
+            id="taxId"
+            placeholder="8026906145"
+            value={taxId}
+            onChange={(e) => {
+              handleSetValue('taxId', e.target.value);
+            }}
+          />
+        </InfoFormControl>
+        <InfoFormControl xs={12} label="Địa chỉ nhà thuốc/phòng khám" htmlFor="address">
+          <InfoInput
+            id="address"
+            placeholder="11 Ngô Quyền, Tân Lợi, Tp. Buôn Ma Thuột, Đắk Lắk"
+            value={bussinessAddress}
+            onChange={(e) => {
+              handleSetValue('bussinessAddress', e.target.value);
+            }}
+          />
+        </InfoFormControl>
+      </Grid>
 
-    <GroupAddressSelect
-      idProvince="billProvince"
-      province={billProvince}
-      idDistrict="billDistrict"
-      district={billDistrict}
-      idWard="billWard"
-      ward={billWard}
-      handleSetValue={handleSetValue}
-    />
-  </Paper>
-);
+      <GroupAddressSelect
+        idProvince="billProvince"
+        province={billProvince}
+        idDistrict="billDistrict"
+        district={billDistrict}
+        idWard="billWard"
+        ward={billWard}
+        handleSetValue={handleSetValue}
+      />
+    </Paper>
+  );
+};
 
 export default EnterpriseForm;
--- src/pages/api/districts/[id]/wards/index.js
+++ src/pages/api/districts/[id]/wards/index.js
@@ -4,7 +4,7 @@ export default (req, res) => {
   const { id } = req.query;
   res.statusCode = 200;
   const d = wards.filter((ward) => String(ward.huyen_id) === id);
-  let data = [{ id: 0, huyen_id: 0, name: 'Chọn Quận/Huyện...' }];
+  let data = [{ id: 0, huyen_id: 0, name: 'Chọn Phường/Xã...' }];
   data = data.concat(d);
 
   res.json({
