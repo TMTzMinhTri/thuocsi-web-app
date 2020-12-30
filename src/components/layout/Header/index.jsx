ed2fa0f60ee56891593fbb8e87f727010b73876a
NHT <truongh.nguyen@thuocsi.vn>
2020-12-30 14:57:24 +0700

    add search input to header

--- src/components/layout/Header/index.jsx
+++ src/components/layout/Header/index.jsx
@@ -1,12 +1,13 @@
 import React, { memo, useCallback } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import { useModal } from 'hooks';
-
-import { CardTravel, House, NewReleases } from '@material-ui/icons';
+import { IconButton } from '@material-ui/core';
+import { CardTravel, House, NewReleases, NotificationsNoneOutlined } from '@material-ui/icons';
 import { PATH_NEWS, PATH_CAREER, PATH_SUPPLIER } from 'constants/Paths';
 import { LOGO_THUOCSI } from 'constants/Images';
 import { SignUpModal, SignInModal, ForgetPasswordModal } from 'components/organisms';
-import { HeaderUser } from 'components/mocules';
+import { HeaderUser, SearchInput } from 'components/mocules';
 import { useAuth } from 'context';
 import { LinkComp, Button } from '../../atoms';
 import styles from './styles.module.css';
@@ -49,9 +50,9 @@ const InfoHeader = memo(() => {
         </div>
       </div>
       <div className={styles.login}>
-        <div className={styles.logo}>
-          <Image src={LOGO_THUOCSI} width="164px" height="45px" />
-        </div>
+        <Link href="/">
+          <Image className={styles.logo} href="/" src={LOGO_THUOCSI} width="164px" height="45px" />
+        </Link>
         {!isAuthenticated ? (
           <>
             <SignInModal
@@ -76,7 +77,16 @@ const InfoHeader = memo(() => {
             </div>
           </>
         ) : (
-          <HeaderUser user={user} />
+          <>
+            <SearchInput className={styles.SearchInput} />
+            <div className={styles.rSection}>
+              <IconButton className={styles.notiIcon}>
+                <NotificationsNoneOutlined />
+                {/* <FontAwesomeIcon icon={faBell} /> */}
+              </IconButton>
+              <HeaderUser user={user} />
+            </div>
+          </>
         )}
       </div>
     </div>
