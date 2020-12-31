5ad4c0e6c317b2e93b683e2b67af5bc7ffbaf861
An Pham <an.pham@thuocsi.vn>
2020-12-31 15:39:34 +0700

    fix slider

--- src/pages/api/banner/index.js
+++ src/pages/api/banner/index.js
@@ -8,15 +8,14 @@ export default (req, res) => {
       {
         id: '1',
         alt: 'Ngày Đái Tháo Đường 14.11',
-        image: 'https://images.thuocsi.vn/PYZZvbdYLGrV1NWC9L8a4uQh',
+        image: '/images/slider/slider.jpg',
         link: '',
       },
       {
         id: '2',
-        alt: 'Xuất hóa đơn đỏ',
-        image: 'https://images.thuocsi.vn/PYZZvbdYLGrV1NWC9L8a4uQh',
-        link:
-          'https://thuocsi.zendesk.com/hc/vi/articles/360033568172-C%C3%A1ch-y%C3%AAu-c%E1%BA%A7u-xu%E1%BA%A5t-h%C3%B3a-%C4%91%C6%A1n-%C4%91%E1%BB%8F-t%E1%BA%A1i-thuocsi-vn-',
+        alt: 'Ngày Đái Tháo Đường 14.11',
+        image: '/images/slider/slider.jpg',
+        link: '',
       },
     ],
   });
