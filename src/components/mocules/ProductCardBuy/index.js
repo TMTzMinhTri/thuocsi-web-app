c218ee601ad55523005517a4febdd4d3b91c947a
An Pham <an.pham@thuocsi.vn>
2021-01-05 15:04:27 +0700

    hide price show price when login

--- src/components/mocules/ProductCardBuy/index.js
+++ src/components/mocules/ProductCardBuy/index.js
@@ -1,15 +1,18 @@
 import React, { useState, useCallback } from 'react';
 import { CardActions, Typography, Box, IconButton } from '@material-ui/core';
+import { Button as CustomButton } from 'components';
 import { Delete } from '@material-ui/icons';
 import formatCurrency from 'utils/FormarCurrency';
 import clsx from 'clsx';
 import useModal from 'hooks/useModal';
-import { useCart } from 'context';
+import { useCart, useAuth } from 'context';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { debounce } from 'lodash';
 import { MinusButton, PlusButton, InputProduct } from '../../atoms';
 import DealSection from '../DealSection';
 import RemoveProductModal from '../../organisms/RemoveProductModal';
+import SignInModal from '../../organisms/SignInModal';
+import ForgetPasswordModal from '../../organisms/ForgetPasswordModal';
 import styles from './styles.module.css';
 
 const ProductCardBuy = ({
@@ -28,10 +31,21 @@ const ProductCardBuy = ({
   product,
 }) => {
   const [value, setValue] = useState(product.quantity || 0);
+  const [isShowingLogin, toggleLogin] = useModal();
+  const [isShowingForgetPassword, toggleForgetPassword] = useModal();
+  const { isAuthenticated } = useAuth();
   const [isShowModalRemove, toggleRemove] = useModal();
   const { increase, decrease, removeProduct, increaseBy } = useCart();
-  const removeProductOutCart = () => { toggleRemove(); };
-  const handleRemove = () => { removeProduct(product); };
+  const handleChangeForget = useCallback(() => {
+    toggleLogin();
+    toggleForgetPassword();
+  }, [toggleLogin, toggleForgetPassword]);
+  const removeProductOutCart = () => {
+    toggleRemove();
+  };
+  const handleRemove = () => {
+    removeProduct(product);
+  };
   const handleDecrease = () => {
     if (cart && product.quantity < 2) return;
     decrease(product);
@@ -52,11 +66,14 @@ const ProductCardBuy = ({
     setValue(product.quantity || 0);
   };
 
-  const handler = useCallback(debounce((val) => handleOnIncreaseBy(val), 2000), []);
+  const handler = useCallback(
+    debounce((val) => handleOnIncreaseBy(val), 2000),
+    [],
+  );
 
   const handleInputChange = (e) => {
     const curValue = e.target.value || 0;
-    if ((/^\d+$/.test(curValue)) && curValue < 1000) {
+    if (/^\d+$/.test(curValue) && curValue < 1000) {
       setValue(curValue);
       handler(curValue);
     }
@@ -81,57 +98,85 @@ const ProductCardBuy = ({
         </Box>
       ) : (
         <>
-          {hasEvent ? (
-            <Box
-              className={
-                row ? styles.price_wrapper : clsx(styles.price_wrapper, styles.price_wrapper_column)
-              }
-            >
-              <Typography className={styles.deal_price}>{formatCurrency(price)}</Typography>
-              <Typography className={styles.old_price}>{formatCurrency(price)}</Typography>
-            </Box>
+          {isAuthenticated ? (
+            <>
+              {hasEvent ? (
+                <Box
+                  className={
+                    row
+                      ? styles.price_wrapper
+                      : clsx(styles.price_wrapper, styles.price_wrapper_column)
+                  }
+                >
+                  <Typography className={styles.deal_price}>{formatCurrency(price)}</Typography>
+                  <Typography className={styles.old_price}>{formatCurrency(price)}</Typography>
+                </Box>
+              ) : (
+                <Box
+                  className={
+                    row
+                      ? styles.price_wrapper
+                      : clsx(styles.price_wrapper, styles.price_wrapper_column)
+                  }
+                >
+                  <Typography className={styles.deal_price}>{formatCurrency(price)}</Typography>
+                </Box>
+              )}
+              {maxProduct > 0 && (
+                <Typography
+                  className={
+                    row ? styles.text_danger : clsx(styles.text_danger_column, styles.text_danger)
+                  }
+                >
+                  Đặt tối đa {price} sản phẩm
+                </Typography>
+              )}
+              <CardActions
+                className={
+                  row
+                    ? styles.product_action
+                    : clsx(styles.product_action, styles.product_action_column)
+                }
+              >
+                <MinusButton onClick={handleDecrease} />
+                <InputProduct
+                  product={product}
+                  id={id}
+                  onChange={handleInputChange}
+                  searchInput={searchInput}
+                  type={type}
+                  value={value}
+                  name={name}
+                />
+                <PlusButton onClick={() => handleIncrease()} />
+                {cart && (
+                  <IconButton onClick={removeProductOutCart}>
+                    <Delete className={styles.icon} />
+                  </IconButton>
+                )}
+              </CardActions>
+            </>
           ) : (
-            <Box
-              className={
-                row ? styles.price_wrapper : clsx(styles.price_wrapper, styles.price_wrapper_column)
-              }
-            >
-              <Typography className={styles.deal_price}>{formatCurrency(price)}</Typography>
-            </Box>
+            <div className={styles.view_signin_btn}>
+              <CustomButton
+                color="#00b46e"
+                backgroundColor="#fff"
+                className={styles.signin_btn}
+                onClick={toggleLogin}
+              >
+                Đăng nhập để xem giá
+              </CustomButton>
+              <SignInModal
+                visible={isShowingLogin}
+                onClose={toggleLogin}
+                onChangeForget={handleChangeForget}
+              />
+              <ForgetPasswordModal
+                visible={isShowingForgetPassword}
+                onClose={toggleForgetPassword}
+              />
+            </div>
           )}
-          {maxProduct > 0 && (
-          <Typography
-            className={
-                row ? styles.text_danger : clsx(styles.text_danger_column, styles.text_danger)
-              }
-          >
-            Đặt tối đa {price} sản phẩm
-          </Typography>
-          )}
-          <CardActions
-            className={
-              row
-                ? styles.product_action
-                : clsx(styles.product_action, styles.product_action_column)
-            }
-          >
-            <MinusButton onClick={handleDecrease} />
-            <InputProduct
-              product={product}
-              id={id}
-              onChange={handleInputChange}
-              searchInput={searchInput}
-              type={type}
-              value={value}
-              name={name}
-            />
-            <PlusButton onClick={() => handleIncrease()} />
-            {cart && (
-            <IconButton onClick={removeProductOutCart}>
-              <Delete className={styles.icon} />
-            </IconButton>
-            )}
-          </CardActions>
         </>
       )}
       <RemoveProductModal
