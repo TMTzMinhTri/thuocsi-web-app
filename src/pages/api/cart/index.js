3b9d2c4823c1c4ac87b2b5bd27b0fe554a47ce6b
An Pham <an.pham@thuocsi.vn>
2021-01-11 16:52:13 +0700

    remove data mock cart

--- src/pages/api/cart/index.js
+++ src/pages/api/cart/index.js
@@ -6,9 +6,17 @@ export default (req, res) => {
     staus: 'ok',
     data: {
       userID: 'anpham',
-      voucher: '',
-      sku: 'SP01',
-      product: [],
+      product: [
+        // {
+        //   brand: 'Hàng nội địa',
+        //   imageUrl: 'https://assets.thuocsi.vn/assets/defaults/missing-e9cfa4812c342b9780b61700d2ade43591b3c5992f4dffedaa542c07d854b602.png',
+        //   type: 'hộp',
+        //   name: 'Khẩu trang y tế',
+        //   price: 35000,
+        //   sku: 'SP01',
+        //   quantity: 1,
+        // },
+      ],
     },
   });
 };
