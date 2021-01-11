f0b378fd1841f7074a058180dbef3eeaf447259e
An Pham <an.pham@thuocsi.vn>
2021-01-11 11:00:07 +0700

    update pages

--- src/pages/users/referrals/index.js
+++ src/pages/users/referrals/index.js
@@ -1,4 +1,4 @@
-import { Template, NavBar, Header, ReferralList, InfoContainer } from 'components';
+import { Template, NavBar, Header, ReferralList, InfoContainer, HeaderMobile } from 'components';
 import { Container } from '@material-ui/core';
 import { AuthClient, CustomerClient } from 'clients';
 
@@ -34,16 +34,19 @@ export async function getServerSideProps() {
   }
 }
 
-const MyReferral = ({ mostResearched = [], wallet, referrals = [] }) => {
+const MyReferral = ({ mostResearched = [], wallet, referrals = [], isMobile }) => {
   const title = 'Giới thiệu bạn bè – Đặt thuốc sỉ rẻ hơn tại thuocsi.vn';
   return (
-    <Template title={title}>
-      <Header />
+    <Template title={title} isMobile={isMobile}>
+      {isMobile ? <HeaderMobile title="Giới thiệu bạn bè" /> : <Header />}
+      {!isMobile
+      && (
       <NavBar
         mostResearched={mostResearched}
         point={wallet.loyaltyPoint}
         balance={wallet.balance}
       />
+      )}
       <div style={{ backgroundColor: '#f4f7fc' }}>
         <Container maxWidth="lg">
           <InfoContainer value={3} title="Giới thiệu bạn bè" wallet={wallet}>
--- src/pages/users/user-promo-codes/index.js
+++ src/pages/users/user-promo-codes/index.js
@@ -1,4 +1,4 @@
-import { Template, NavBar, Header, PromoList, InfoContainer } from 'components';
+import { Template, NavBar, Header, PromoList, InfoContainer, HeaderMobile } from 'components';
 import { Container } from '@material-ui/core';
 import { AuthClient, CustomerClient } from 'clients';
 
@@ -34,16 +34,19 @@ export async function getServerSideProps() {
   }
 }
 
-const MyReferral = ({ mostResearched = [], wallet, promos = [] }) => {
+const MyReferral = ({ mostResearched = [], wallet, promos = [], isMobile }) => {
   const title = 'Mã giảm giá của tôi – Đặt thuốc sỉ rẻ hơn tại thuocsi.vn';
   return (
-    <Template title={title}>
-      <Header />
+    <Template title={title} isMobile={isMobile}>
+      {isMobile ? <HeaderMobile title="Mã giảm giá" /> : <Header />}
+      {!isMobile
+      && (
       <NavBar
         mostResearched={mostResearched}
         point={wallet.loyaltyPoint}
         balance={wallet.balance}
       />
+      )}
       <div style={{ backgroundColor: '#f4f7fc' }}>
         <Container maxWidth="lg">
           <InfoContainer value={4} title="Mã giảm giá của tôi" wallet={wallet}>
