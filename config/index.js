38f7f95cc26445dfb908198b012e8bc4d03e7c11
NHT <truongh.nguyen@thuocsi.vn>
2021-01-13 15:22:41 +0700

    fill info user

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
-export const API_HOST = process.env.API_HOST || '/backend';
+export const API_HOST = process.env.API_HOST || 'https://api.v2-dev.thuocsi.vn';
 export const MOCK_API_HOST = process.env.MOCK_API_HOST || '/api';
 export const BASIC_AUTHEN = 'Basic Q1VTVE9NRVIvMTIxODMxODMxNzoxMjE4MzE4MzE3';
