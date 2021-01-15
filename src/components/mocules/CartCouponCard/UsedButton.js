4aa96f0b3b6473c80626b78ab4a1e0bb49dfa138
thuocsi_datle <dat.le@thuocsi.vn>
2021-01-15 11:53:51 +0700

    fix used buttonin cart coupon card

--- src/components/mocules/CartCouponCard/UsedButton.js
+++ src/components/mocules/CartCouponCard/UsedButton.js
@@ -2,15 +2,15 @@ import styled from 'styled-components';
 import { Button } from 'components/atoms';
 
 const StyledUsedButton = styled(Button)`
-margin-top: 20px !important;
-margin-left: 22px !important;
-text-transform: none !important;
-color: #fff !important;
-background-color: #00b46e !important;
-border: 1px solid #00b46e !important;
-padding: 4px 10px !important;
-transition: 0.5s;
-opacity: 0.65;
+    margin-top: 20px !important;
+    margin-left: 22px !important;
+    text-transform: none !important;
+    color: #fff !important;
+    background-color: #00b46e !important;
+    border: 1px solid #00b46e !important;
+    padding: 4px 10px !important;
+    transition: 0.5s;
+    opacity: 0.65;
 `;
 
 export default StyledUsedButton;
