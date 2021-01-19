3dea4f03169168a6538eb0cfb10899aa877ef3fd
Nguyen Thuan <nguyenchinhthuan15@gmail.com>
2021-01-19 18:39:29 +0700

    prevent  duplicate toast

--- src/utils/FormarCurrency.js
+++ src/utils/FormarCurrency.js
@@ -1,5 +1,5 @@
 function formatCurrency(n, separate = '.') {
-  const s = (n).toString();
+  const s = n.toString();
   const regex = /\B(?=(\d{3})+(?!\d))/g;
   const ret = `${s.replace(regex, separate)} đ`;
   return ret;
