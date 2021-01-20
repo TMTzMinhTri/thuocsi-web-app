4a6b80abac803bdea39425dfc2d401f83fe444de
Nguyen Thuan <nguyenchinhthuan15@gmail.com>
2021-01-20 13:51:03 +0700

    Remove data from notification mock api

--- src/pages/api/mock/notifications/index.js
+++ src/pages/api/mock/notifications/index.js
@@ -4,49 +4,6 @@ export default (req, res) => {
   res.statusCode = 200;
   res.json({
     status: 'OK',
-    data: [
-      {
-        id: 1,
-        title: 'Xin chào, đơn hàng #197359 của bạn đã được xác nhận và đang được xử lý. Nhấp vào đây (hoặc vào mục Đơn hàng của tôi) để xem chi tiết đơn hàng.',
-        create_at: '2021-01-08T09:22:54.677Z',
-        slug: '/my-order/197180',
-      },
-      {
-        id: 2,
-        title: 'Xin chào, đơn hàng #197359 của bạn đã được xác nhận và đang được xử lý. Nhấp vào đây (hoặc vào mục Đơn hàng của tôi) để xem chi tiết đơn hàng.',
-        create_at: '2021-01-08T09:22:54.677Z',
-        slug: '/my-order/197180',
-      },
-      {
-        id: 3,
-        title: 'Xin chào, đơn hàng #197359 của bạn đã được xác nhận và đang được xử lý. Nhấp vào đây (hoặc vào mục Đơn hàng của tôi) để xem chi tiết đơn hàng.',
-        create_at: '2021-01-08T09:22:54.677Z',
-        slug: '/my-order/197180',
-      },
-      {
-        id: 4,
-        title: 'Xin chào, đơn hàng #197359 của bạn đã được xác nhận và đang được xử lý. Nhấp vào đây (hoặc vào mục Đơn hàng của tôi) để xem chi tiết đơn hàng.',
-        create_at: '2021-01-08T09:22:54.677Z',
-        slug: '/my-order/197180',
-      },
-      {
-        id: 5,
-        title: 'Xin chào, đơn hàng #197359 của bạn đã được xác nhận và đang được xử lý. Nhấp vào đây (hoặc vào mục Đơn hàng của tôi) để xem chi tiết đơn hàng.',
-        create_at: '2021-01-08T09:22:54.677Z',
-        slug: '/my-order/197180',
-      },
-      {
-        id: 6,
-        title: 'Xin chào, đơn hàng #197359 của bạn đã được xác nhận và đang được xử lý. Nhấp vào đây (hoặc vào mục Đơn hàng của tôi) để xem chi tiết đơn hàng.',
-        create_at: '2021-01-08T09:22:54.677Z',
-        slug: '/my-order/197180',
-      },
-      {
-        id: 7,
-        title: 'Xin chào, đơn hàng #197359 của bạn đã được xác nhận và đang được xử lý. Nhấp vào đây (hoặc vào mục Đơn hàng của tôi) để xem chi tiết đơn hàng.',
-        create_at: '2021-01-08T09:22:54.677Z',
-        slug: '/my-order/197180',
-      },
-    ],
+    data: [],
   });
 };
