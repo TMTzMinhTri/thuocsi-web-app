e6011069d865f00841fb1f26ae730ffdce708544
An Pham <an.pham@thuocsi.vn>
2021-01-21 17:36:17 +0700

    add paging on quick order

--- src/components/organisms/QuickOrderList/index.js
+++ src/components/organisms/QuickOrderList/index.js
@@ -1,15 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Box } from '@material-ui/core';
 import { SearchClient } from 'clients';
 import debounce from 'utils/debounce';
+import { Pagination } from '@material-ui/lab';
+import { useRouter } from 'next/router';
 import ProductCardHorizontal from '../ProductCardHorizontal';
+import { PAGE_SIZE } from '../../../constants/data';
 import { SearchOrder } from '../../mocules';
 
 import styles from './style.module.css';
 
-const QuickOrderList = ({ products, isMobile }) => {
+const QuickOrderList = ({ products, isMobile, page, total }) => {
   const [searchProduct, setSearchProduct] = useState(products);
+  const router = useRouter();
   const [keyword, setKeyword] = useState('');
+  const [numPage, setNumPage] = useState(page);
+  const pages = Math.ceil(total / PAGE_SIZE);
+
+  useEffect(() => {
+    setSearchProduct(products);
+  }, [products]);
 
   const handler = debounce((cb) => cb(), 500);
   const handleSearchbox = (e) => {
@@ -28,6 +38,15 @@ const QuickOrderList = ({ products, isMobile }) => {
       handler(fetchData);
     }
   };
+
+  const handleChangePage = (event, value) => {
+    if (page === value) return;
+    router.push({
+      pathname: '/quick-order',
+      query: { page: value },
+    });
+    setNumPage(value);
+  };
   return (
     <>
       {!isMobile
@@ -36,11 +55,24 @@ const QuickOrderList = ({ products, isMobile }) => {
       <SearchOrder onSearch={handleSearchbox} />
     </Box>
     )}
-      {searchProduct ? searchProduct.map((item) => (
-        <ProductCardHorizontal key={item.id} product={item} />
-      )) : (
-        <div className={styles.notFound}>Không có sản phẩm với từ khóa {`"${keyword}"`}</div>
-      )}
+      {searchProduct
+        ? (
+          <>
+            {searchProduct.map((item) => (
+              <ProductCardHorizontal key={item.id} product={item} />
+            ))}
+            <div className={styles.pagging}>
+              <Pagination
+                count={pages}
+                defaultPage={numPage}
+                boundaryCount={2}
+                onChange={handleChangePage}
+              />
+            </div>
+          </>
+        ) : (
+          <div className={styles.notFound}>Không có sản phẩm với từ khóa {`"${keyword}"`}</div>
+        )}
     </>
   );
 };
