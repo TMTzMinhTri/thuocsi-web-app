84da7c0adfe005a6243d37c6e034f36b7f17c5da
Nguyen Chinh Thuan <thuan.nguyen@thuocsi.vn>
2021-01-21 13:20:24 +0700

    Fix custom modal

--- src/components/organisms/ProductCart/index.js
+++ src/components/organisms/ProductCart/index.js
@@ -79,7 +79,7 @@ const ProductCart = React.memo((props) => {
         visible={isShowModalWarning}
         title="Xin xác nhận"
         content="Số lượng sản phẩm được đánh dấu quan trọng không được nhiều hơn 20% tổng số sản phẩm"
-        btnOk="OK"
+        btnOnClose="OK"
       />
     </Box>
   );
