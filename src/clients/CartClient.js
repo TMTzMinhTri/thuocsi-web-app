5315ac5bd4c54b8343adbabceb3296b076b44f1c
An Pham <an.pham@thuocsi.vn>
2021-01-21 11:09:42 +0700

    get info cart item from API

--- src/clients/CartClient.js
+++ src/clients/CartClient.js
@@ -1,7 +1,8 @@
-import { GET, POST, PUT, isValid } from './Clients';
+import { PRODUCT_API, CART_API } from 'constants/APIUri';
+import { GET, POST, PUT, isValid, isValidWithData } from './Clients';
 
 async function loadDataCart(ctx) {
-  const res = await GET({ url: '/marketplace/order/v1/cart', ctx });
+  const res = await GET({ url: CART_API.CART_INFO, ctx });
   if (!isValid(res)) {
     return [];
   }
@@ -13,28 +14,54 @@ async function updateCartItem(data) {
     sku: data.product.skuId,
     quantity: data.q,
   };
-  return POST({ url: '/marketplace/order/v1/cart/add', body });
+  return POST({ url: CART_API.CART_ADD, body });
+}
+
+async function getInfoCartItem(data) {
+  const arraySku = [];
+  data.forEach((item) => {
+    arraySku.push(item.sku);
+  });
+  const body = {
+    codes: arraySku,
+  };
+  const res = await POST({ url: PRODUCT_API.PRODUCT_LIST, body });
+  if (!isValidWithData(res)) {
+    return [];
+  }
+  const result = [];
+  data.forEach((item, index) => {
+    result[index] = {
+      ...item,
+      imageUrls: res.data[index] && res.data[index].imageUrls && res.data[index].imageUrls,
+      unit: res.data[index] && res.data[index].unit && res.data[index].unit,
+      volume: res.data[index] && res.data[index].volume && res.data[index].volume,
+      name: res.data[index] && res.data[index].name && res.data[index].name,
+      maxQuantity: res.data[index] && res.data[index].maxQuantity && res.data[index].maxQuantity,
+    };
+  });
+  return result;
 }
 
 async function removeCartItem(data) {
   const body = {
     skuId: data.skuId,
   };
-  return PUT({ url: '/marketplace/order/v1/cart/remove', body });
+  return PUT({ url: CART_API.CART_REMOVE, body });
 }
 
 async function updateRedeemCode(code) {
   const body = {
     redeemCode: code,
   };
-  return PUT({ url: '/marketplace/order/v1/cart', body });
+  return PUT({ url: CART_API.CART_INFO, body });
 }
 
 async function updateNote(note) {
   const body = {
     note,
   };
-  return PUT({ url: '/marketplace/order/v1/cart', body });
+  return PUT({ url: CART_API.CART_INFO, body });
 }
 
 export default {
@@ -43,4 +70,5 @@ export default {
   removeCartItem,
   updateRedeemCode,
   updateNote,
+  getInfoCartItem,
 };
