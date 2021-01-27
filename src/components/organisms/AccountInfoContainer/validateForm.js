c19a37d4cdc6c27ebf1a299c0e213a91755a2646
thuocsi_datle <dat.le@thuocsi.vn>
2021-01-27 11:24:32 +0700

    fix validate

--- src/components/organisms/AccountInfoContainer/validateForm.js
+++ src/components/organisms/AccountInfoContainer/validateForm.js
@@ -16,11 +16,10 @@ const validateForm = ({
   if (provinceCode === '0') throw Error('Tỉnh/Thành Phố không được trống');
   if (districtCode === '0') throw Error('Quận/Huyện không được trống');
   if (wardCode === '0') throw Error('Phường/Xã phố không được trống');
-  if(err) return err;
 
-  if (!ValidateUtils.validatePhone(phone)) err.phone = 'Số điện thoại sai định dạng';
+  if (!ValidateUtils.isEmpty(phone) && !ValidateUtils.validatePhone(phone)) err.phone = 'Số điện thoại sai định dạng';
   if (!ValidateUtils.isEmpty(email) && !ValidateUtils.validateEmail(email)) err.email = 'Email sai định dạng';
-  if (!ValidateUtils.isNumber(mst)) err.mst = 'Mã số thuế sai định dạng';
+  if (!ValidateUtils.isEmpty(mst) && !ValidateUtils.isNumber(mst)) err.mst = 'Mã số thuế sai định dạng';
   return err;
 };
 
