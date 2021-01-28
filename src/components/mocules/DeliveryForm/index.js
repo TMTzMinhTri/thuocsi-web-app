bacb7995c8a7fe26c4b4df87e7e6812c49b49f37
Đạt Duy <dat.le@thuocsi.vn>
2021-01-28 04:28:18 +0000

    Fix/fix group address select

--- src/components/mocules/DeliveryForm/index.js
+++ src/components/mocules/DeliveryForm/index.js
@@ -5,7 +5,7 @@ import InfoInput from '../InfoInput';
 import GroupAddressSelect from '../GroupAddressSelect';
 import styles from './styles.module.css';
 
-const DeliveryForm = ({ province, district, ward, handleSetValue }) => (
+const DeliveryForm = ({ province, district, ward, handleSetValue, handleChangeAddress }) => (
   <Paper className={styles.root} elevation={4}>
     <h1 className={styles.title}> Thông tin giao hàng </h1>
     <Grid container spacing={2}>
@@ -21,6 +21,7 @@ const DeliveryForm = ({ province, district, ward, handleSetValue }) => (
       idWard="ward"
       ward={ward}
       handleSetValue={handleSetValue}
+      handleChangeAddress={handleChangeAddress}
     />
   </Paper>
 );
