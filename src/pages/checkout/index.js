8734247cf0a1fa8a3e9eaeac8cafd0aea3bed26e
NHT <truongh.nguyen@thuocsi.vn>
2021-01-29 09:20:49 +0700

    fix error when access checkout page

--- src/pages/checkout/index.js
+++ src/pages/checkout/index.js
@@ -48,7 +48,7 @@ const CheckoutPage = ({ user = {}, isMobile, cart }) => {
     return <LoadingScreen />;
   }
 
-  const { note: noteValue } = cart[0] || '';
+  const { note: noteValue } = (cart && cart[0]) || {};
 
   const title = `${itemCount} Sản phẩm trong giỏ hàng nhé!`;
   const [checkCondition, setCheckCondition] = React.useState({
