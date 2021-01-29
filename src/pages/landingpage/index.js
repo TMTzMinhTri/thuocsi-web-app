4edaea5691441d23397d386c566944261276226b
Nguyen Chinh Thuan <thuan.nguyen@thuocsi.vn>
2021-01-29 14:53:43 +0700

    Cache landing page

--- src/pages/landingpage/index.js
+++ src/pages/landingpage/index.js
@@ -4,21 +4,21 @@ import Template from 'components/layout/Template';
 import { v4 as uuidv4 } from 'uuid';
 import { useAuth } from 'context';
 
+const DynamicWhyBuymed = dynamic(() => import('components/organisms/WhyBuymed'));
+const DynamicCommonQuestion = dynamic(() => import('components/mocules/CommonQuestion'));
+const DynamicPartners = dynamic(() => import('components/organisms/Partners'));
+const DynamicSliderComp = dynamic(() => import('components/organisms/SliderComp'));
+const DynamicMedia = dynamic(() => import('components/organisms/Media'));
+const DynamicHomeCTASection = dynamic(() => import('components/organisms/HomeCTASection'));
+const DynamicBannerSlider = dynamic(() => import('components/organisms/BannerSlider'));
+const DynamicProductSlider = dynamic(() => import('components/organisms/ProductSliderSection'));
+
 export default function LandingPage(props) {
   const { isAuthenticated } = useAuth();
   const { infoBanner = [], isMobile, blocks = [] } = props;
   const title = 'Tra cứu và đặt thuốc giá sỉ nhanh tại thuocsi.vn';
   const pageName = 'home';
   const pageTitle = 'Trang chủ';
-
-  const DynamicWhyBuymed = dynamic(() => import('components/organisms/WhyBuymed'));
-  const DynamicCommonQuestion = dynamic(() => import('components/mocules/CommonQuestion'));
-  const DynamicPartners = dynamic(() => import('components/organisms/Partners'));
-  const DynamicSliderComp = dynamic(() => import('components/organisms/SliderComp'));
-  const DynamicMedia = dynamic(() => import('components/organisms/Media'));
-  const DynamicHomeCTASection = dynamic(() => import('components/organisms/HomeCTASection'));
-  const DynamicBannerSlider = dynamic(() => import('components/organisms/BannerSlider'));
-  const DynamicProductSlider = dynamic(() => import('components/organisms/ProductSliderSection'));
 
   return (
     <Template title={title} isMobile={isMobile} pageName={pageName} pageTitle={pageTitle}>
