22f071f5540ee25ac42c8af471c8532bf4e8585e
thuocsi_datle <dat.le@thuocsi.vn>
2021-02-02 16:47:39 +0700

    done fix promo

--- src/pages/my-account/index.js
+++ src/pages/my-account/index.js
@@ -8,7 +8,6 @@ export async function getServerSideProps(ctx) {
 }
 
 const MyAccount = ({ user, isMobile }) => {
-  console.log(user)
   const title = 'Cập nhật hồ sơ – Đặt thuốc sỉ rẻ hơn tại thuocsi.vn';
   return (
     <Template title={title} isMobile={isMobile}>
