dd9e882507135f758d617f1ac5ca063c392df58f
An Pham <an.pham@thuocsi.vn>
2021-02-03 17:23:45 +0700

    resolve the conflict

--- src/pages/promo-codes/index.js
+++ src/pages/promo-codes/index.js
@@ -20,8 +20,9 @@ export async function getServerSideProps(ctx) {
 const PromoCodes = ({ promos = [], isMobile }) => {
   const title = 'Mã giảm giá – Đặt thuốc sỉ rẻ hơn tại thuocsi.vn';
   const pageTitle = 'Mã giảm giá';
+  const name = 'promo-codes';
   return (
-    <Template title={title} isMobile={isMobile} pageTitle={pageTitle}>
+    <Template title={title} isMobile={isMobile} pageTitle={pageTitle} pageName={name}>
       <div style={{ backgroundColor: '#f4f7fc', minHeight: '80vh' }}>
         <PromoCodesContainer promos={promos} />
       </div>
