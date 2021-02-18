aa593f893d029cbed731418ff33eb143c9d34464
Nguyễn Chính Thuận <thuan.nguyen@thuocsi.vn>
2021-02-18 22:18:08 +0700

    Update fb pixel

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -25,7 +25,8 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 
 import { MOBILE } from 'constants/Device';
 
-import { fbpixel, gtag } from 'utils';
+import * as fbpixel from 'utils/fbpixel';
+import * as gtag from 'utils/gtag';
 
 const NAMESPACE_REQUIRED_DEFAULT = 'common';
 
