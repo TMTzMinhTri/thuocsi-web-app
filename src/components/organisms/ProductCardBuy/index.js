6c69f1b7cc267de8ec65677561b92eb0c72b1114
NHT <truongh.nguyen@thuocsi.vn>
2021-03-12 11:13:29 +0700

    remove console

--- src/components/organisms/ProductCardBuy/index.js
+++ src/components/organisms/ProductCardBuy/index.js
@@ -45,7 +45,6 @@ const ProductCardBuy = ({
   const removeProductOutCart = () => {
     toggleRemove();
   };
-  console.log(deal.maxQuantity, deal.quantity);
   const canDeleteProduct = () => {
     const quantityAfterDel = cartItems.length - 1;
     let importantQuantity = importantList.length;
