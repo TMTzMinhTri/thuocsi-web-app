f28fc384a2bb5345a372529e7eb442a18ecc0bc1
An Pham <an.pham@thuocsi.vn>
2021-03-26 11:22:57 +0700

    fix bug modal

--- src/components/atoms/Modal/index.js
+++ src/components/atoms/Modal/index.js
@@ -23,6 +23,11 @@ const StyledModal = styled(ModalItem)`
   padding: 15px;
   &.signup-modal .auth-modal-content{
     height: 100%;
+    max-height: 780px;
+  }
+  &.signup-modal .auth-modal-body{
+    overflow: auto;
+    height: calc(100% - 50px);
   }
 `;
 
