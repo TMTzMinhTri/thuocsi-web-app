5ef0c8c7bce933db79b466a67fbb7389885793f2
Nhan Tran <nhan.tran@thuocsi.vn>
2021-03-28 02:04:49 +0700

    Add token into iframe

--- src/pages/discovery/index.js
+++ src/pages/discovery/index.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import Template from 'components/layout/Template';
 import { DISCOVERY_URL } from 'sysconfig';
+import { getSessionTokenClient } from 'clients';
 import styles from './styles.module.css';
 
 export async function getServerSideProps() {
+  
   return {
     props: {},
   };
 }
 
 const Discovery = ({ isMobile }) => {
+  const token = getSessionTokenClient();
   const pageTitle = 'Khám phá';
   const title = 'Khám phá – Đặt thuốc sỉ rẻ hơn tại thuocsi.vn';
   const name = 'deals';
   return (
     <Template title={title} isMobile={isMobile} pageName={name} pageTitle={pageTitle}>
       <div className={styles.discovery_wrapper}>
-        <iframe title="Discovery" className={styles.iframe} src={DISCOVERY_URL} width="100%" height="100%" />
+        <iframe title="Discovery" className={styles.iframe} src={`${DISCOVERY_URL}?token=${token}`} width="100%" height="100%" />
       </div>
     </Template>
   );
