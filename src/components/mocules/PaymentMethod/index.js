841f8ae107a60c5ea31a7d42214eb5636517f910
thuocsi_datle <dat.le@thuocsi.vn>
2021-04-01 14:45:37 +0700

    format

--- src/components/mocules/PaymentMethod/index.js
+++ src/components/mocules/PaymentMethod/index.js
@@ -14,7 +14,7 @@ const renderPaymentMethod = ({ item }) => {
         key={uuidv4()}
         value={code}
         control={<Radio classes={{ root: clsx(styles.checkbox, styles.checkbox_color) }} />}
-        label={
+        label={(
           <div className={styles.fw500}>
             {name}{' '}
             {code === 'PAYMENT_METHOD_BANK' && url && (
@@ -23,7 +23,7 @@ const renderPaymentMethod = ({ item }) => {
               </a>
             )}
           </div>
-        }
+        )}
       />
       {subTitle && <div className={styles.subTitle}>{subTitle}</div>}
 
