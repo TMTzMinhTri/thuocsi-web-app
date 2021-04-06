3647a77d3c043b991f15c92c239e2a8f9d0975ba
Nguyen Chinh Thuan <thuan.nguyen@thuocsi.vn>
2021-04-06 16:03:27 +0700

    Update link license

--- src/sysconfig/index.js
+++ src/sysconfig/index.js
@@ -11,3 +11,7 @@ export const ENV = process.env.NEXT_PUBLIC_ENV || 'stg';
 export const GG_IMAGE = process.env.NEXT_PUBLIC_DOMAIN_GG_IMAGE || 'https://storage.googleapis.com';
 export const PROXY_IMAGE =
   process.env.NEXT_PUBLIC_DOMAIN_PROXY_IMAGE || 'https://img-proxy.v2-dev.thuocsi.vn';
+
+export const LINK_LICENSE =
+  process.env.NEXT_PUBLIC_LINK_LICENSE ||
+  'https://storage.googleapis.com/buymed-storage/trading_license/1.%20Trading%20License%20-%20Buymed%20(GC%2020%20June%202019)%20(VN).pdf';
