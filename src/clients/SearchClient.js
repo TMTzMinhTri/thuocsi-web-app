c34ac2742281a1e4b94cc32d24d469887ac7dfed
Nguyen Chinh Thuan <thuan.nguyen@thuocsi.vn>
2021-04-06 15:21:39 +0700

    update search fuzzy

--- src/clients/SearchClient.js
+++ src/clients/SearchClient.js
@@ -1,7 +1,8 @@
 import { GET, isValid } from './Clients';
 
+// TODO: update search fuzzy
 async function searchKeywords(keyword) {
-  const url = `/marketplace/product/v1/search?q=${keyword}`;
+  const url = `/marketplace/product/v1/search/fuzzy?q=${keyword}`;
   const res = await GET({ url });
   if (!isValid(res)) {
     return [];
