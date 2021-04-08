d6fb54e6943f1a32d7670828a4a4c7712e1e4a85
An Pham <an.pham@thuocsi.vn>
2021-04-08 17:38:24 +0700

    add rollbar page

--- src/hooks/useRollbar.js
+++ src/hooks/useRollbar.js
@@ -1,11 +1,30 @@
-const { useState } = require('react');
+import Rollbar from 'rollbar';
 
-const useModal = (data = false) => {
-  const [isShowing, setIsShowing] = useState(data);
-  function toggle() {
-    setIsShowing(!isShowing);
-  }
-  return [isShowing, toggle];
+export const SCROLLBAR_KEY = process.env.NEXT_PUBLIC_SCROLLBAR_KEY;
+const getErrorMessage = (rollbar) => {
+  const configure = (payload) => rollbar.configure(payload);
+  const critical = (payload) => rollbar.critical(payload);
+  const error = (payload) => rollbar.error(payload);
+  const warning = (payload) => rollbar.warning(payload);
+  const info = (payload) => rollbar.info(payload);
+  const debug = (payload) => rollbar.debug(payload);
+  return {
+    configure,
+    critical,
+    error,
+    warning,
+    info,
+    debug,
+  };
 };
 
-export default useModal;
+const useRollbar = () => {
+  const rollbar = new Rollbar({
+    accessToken: SCROLLBAR_KEY,
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+  });
+  return getErrorMessage(rollbar);
+};
+
+export default useRollbar;
